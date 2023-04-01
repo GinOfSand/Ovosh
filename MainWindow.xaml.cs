adad9a55abec3629ce6dcb7778ad4f7ca137ec24
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2023-04-01 19:19:11 +0700

    Optimization

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -14,23 +14,72 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Windows.Controls.Primitives;
+using System.Data;
+using System.Runtime.Remoting.Contexts;
 
 namespace Ovosh
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        string conf = null;
+        string conf2 = null;
         SqlConnection conect = null;
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+        }
+        public string W_i_n()
+        {
+            string c = null;
+            Window1 window = new Window1();
+            window.Owner = this;
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            if (window.ShowDialog() == true)
+            {
+                c = window.Color.Text;
+            }
+            return c;
+        }
+        public void Tabler(SqlCommand com)
+        {
+            SqlDataReader rdr = null;
+            try
+            {
+
+                SqlCommand cmd = com;
+
+                DataTable table = new DataTable();
+                if (Tab1.IsSelected)
+                {
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    sda.Fill(table);
+                    DATA_TABLE.ItemsSource = table.DefaultView;
+                }
+
+                if (Tab2.IsSelected)
+                {
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    sda.Fill(table);
+                    DATA_TABLE2.ItemsSource = table.DefaultView;
+                }
+                DATA_TABLE.DataContext = table;
+                rdr.Close();
+            }
+            catch
+            {
+
+            }
+        }
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
-            
+
             try
             {
                 conect = new SqlConnection(@"Data Source = DESKTOP-H6EAR4O\SQLEXPRESS; Initial Catalog = OiF;
@@ -38,63 +87,12 @@ namespace Ovosh
                 conect.Open();
                 MessageBox.Show("Соединение установлено", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
             }
-            catch 
+            catch
             {
-                MessageBox.Show("Ошибка подключения к Базе Данных!","Оибка соединения",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("Ошибка подключения к Базе Данных!", "Оибка соединения", MessageBoxButton.OK, MessageBoxImage.Error);
                 conect = null;
             }
-            
-        }
 
-        private void btnList_Click(object sender, RoutedEventArgs e)
-        {
-            
-           
-            if (conect != null)
-            {
-                
-               int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("select * from Table_O_F", conect);
-                    rdr = cmd.ExecuteReader();
-                    
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        
-                        listB1.Items.Add(string.Format("{0,-5} {1,-15} {2,-15} {3,-25} {4,-17}", rdr.GetName(0), rdr.GetName(1) ,rdr.GetName(2) ,rdr.GetName(3) , rdr.GetName(4)));
-                        while (rdr.Read())
-                        {
-                           
-                            listB1.Items.Add(string.Format("{0,-5} {1,-15} {2,-15} {3,-25} {4,-17}", rdr[0], rdr[1], rdr[2], rdr[3], rdr[4]));
-
-                        }
-                        
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-                        
-                        listB2.Items.Clear();
-                        listB2.Items.Add(string.Format("{0,-5} {1,-15} {2,-15} {3,-25} {4,-17}", rdr.GetName(0), rdr.GetName(1), rdr.GetName(2), rdr.GetName(3), rdr.GetName(4)));
-                        while (rdr.Read())
-                        {
-
-                            listB2.Items.Add(string.Format("{0,-5} {1,-15} {2,-15} {3,-25} {4,-17}", rdr[0], rdr[1], rdr[2], rdr[3], rdr[4]));
-
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
-                
-            }
-            
         }
 
         private void Window_Closing(object sender, CancelEventArgs e)
@@ -104,222 +102,145 @@ namespace Ovosh
                 conect.Close();
                 MessageBox.Show("Соединение разорвано", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
             }
-            
+
         }
+        private void btnList_Click(object sender, RoutedEventArgs e)
+        {
+            Tabler(new SqlCommand("select * from Table_O_F", conect));
+        }
+
+
 
         private void ButtonName_Click(object sender, RoutedEventArgs e)
         {
-            if (conect != null)
-            {
-
-                int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("select id ,Название from Table_O_F", conect);
-                    rdr = cmd.ExecuteReader();
-
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        
-                        listB1.Items.Add(string.Format("{0,-5} {1,-15}", rdr.GetName(0), rdr.GetName(1)));
-                        while (rdr.Read())
-                        {
-                            listB1.Items.Add(string.Format("{0,-5} {1,-15}", rdr[0], rdr[1]));
-                        }
-
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-                        
-                        listB2.Items.Clear();
-                        listB2.Items.Add(string.Format("{0,-5} {1,-15}", rdr.GetName(0), rdr.GetName(1)));
-                        while (rdr.Read())
-                        {
-                            listB2.Items.Add(string.Format("{0,-5} {1,-15}", rdr[0], rdr[1]));
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
-
-            }
+            Tabler(new SqlCommand("select id ,Название from Table_O_F", conect));
 
         }
 
         private void ButtonColor_Click(object sender, RoutedEventArgs e)
         {
-            if (conect != null)
-            {
 
-                int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("select id ,Цвет from Table_O_F", conect);
-                    rdr = cmd.ExecuteReader();
+            Tabler(new SqlCommand("select id ,Цвет from Table_O_F", conect));
 
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        
-                        listB1.Items.Add(string.Format("{0,-5} {1,-15}", rdr.GetName(0), rdr.GetName(1)));
-                        while (rdr.Read())
-                        {
-                            listB1.Items.Add(string.Format("{0,-5} {1,-15}", rdr[0], rdr[1]));
-                        }
-
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-                        
-                        listB2.Items.Clear();
-                        listB2.Items.Add(string.Format("{0,-5} {1,-15}", rdr.GetName(0), rdr.GetName(1)));
-                        while (rdr.Read())
-                        {
-                            listB2.Items.Add(string.Format("{0,-5} {1,-15}", rdr[0], rdr[1]));
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
-
-            }
         }
 
         private void ButtonMAX_Click(object sender, RoutedEventArgs e)
         {
-            if (conect != null)
-            {
 
-                int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность = (SELECT MAX(Колорийность) FROM Table_O_F)", conect);
-                    rdr = cmd.ExecuteReader();
-
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        listB1.Items.Add("Максимальная колорийность");
-                        while (rdr.Read())
-                        {
-                            listB1.Items.Add(string.Format("{0,-15}{1,-5}", rdr[0], rdr[1]));
-                        }
-
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-                        
-                        listB2.Items.Clear();
-                        listB2.Items.Add("Максимальная колорийность"); ;
-                        while (rdr.Read())
-                        {
-                            listB2.Items.Add(string.Format("{0,-15}{1,-5}", rdr[0], rdr[1]));
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
-            }
-        }
+            Tabler(new SqlCommand("SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность = (SELECT MAX(Колорийность) FROM Table_O_F)", conect));  }
 
         private void ButtonMIN_Click(object sender, RoutedEventArgs e)
         {
-            if (conect != null)
-            {
 
-                int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность = (SELECT MIN(Колорийность) FROM Table_O_F)", conect);
-                    rdr = cmd.ExecuteReader();
+            Tabler(new SqlCommand("SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность = (SELECT MIN(Колорийность) FROM Table_O_F)", conect));
 
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        listB1.Items.Add("Минимальная колорийность");
-                        while (rdr.Read())
-                        {
-                            listB1.Items.Add(string.Format("{0,-15}{1,-5}", rdr[0], rdr[1]));
-                        }
-
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-
-                        listB2.Items.Clear();
-                        listB2.Items.Add("Минимальная колорийность"); ;
-                        while (rdr.Read())
-                        {
-                            listB2.Items.Add(string.Format("{0,-15}{1,-5}", rdr[0], rdr[1]));
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
-            }
         }
 
         private void ButtonAVG_Click(object sender, RoutedEventArgs e)
         {
-            if (conect != null)
+
+
+            Tabler(new SqlCommand("SELECT AVG(Колорийность) FROM Table_O_F", conect));
+
+        }
+
+        private void ButtonCout_Ovosh_Click(object sender, RoutedEventArgs e)
+        {
+
+            Tabler(new SqlCommand("SELECT COUNT(*) AS Количество_Овощей FROM Table_O_F WHERE Тип = 'Овощь'", conect));
+
+        }
+        private void ButtonCout_Fruts_Click(object sender, RoutedEventArgs e)
+        {
+
+            Tabler(new SqlCommand("SELECT COUNT(*) AS Количество_Фруктов FROM Table_O_F WHERE Тип = 'Фрукт'", conect));
+
+        }
+
+        private void ButtonColor_Chose_Click(object sender, RoutedEventArgs e)
+        {
+            conf = W_i_n();
+            Tabler(new SqlCommand($"SELECT COUNT(*) AS Выбранного_Цвета FROM Table_O_F WHERE Цвет = '{conf}'", conect));
+
+        }
+
+        private void Button_Count_all_Color_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Tabler(new SqlCommand("SELECT Цвет, COUNT(*) as Количество FROM Table_O_F GROUP BY Цвет", conect));
+
+
+        }
+
+        private void Button_Klori_Menshe_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            conf = W_i_n();
+            int d;
+            if (int.TryParse(conf, out d))
             {
+                Tabler(new SqlCommand($"SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность < {conf}", conect));
 
-                int index = TAB_CONTROL.SelectedIndex;
-                SqlDataReader rdr = null;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("SELECT AVG(Колорийность) FROM Table_O_F", conect);
-                    rdr = cmd.ExecuteReader();
 
-                    if (Tab1.IsSelected)
-                    {
-                        listB1.Items.Clear();
-                        listB1.Items.Add("Средняя колорийность");
-                        while (rdr.Read())
-                        {
-                            listB1.Items.Add(string.Format("{0,-15}", rdr[0]));
-                        }
 
-                    }
-
-                    if (Tab2.IsSelected)
-                    {
-
-                        listB2.Items.Clear();
-                        listB2.Items.Add("Средняя колорийность"); ;
-                        while (rdr.Read())
-                        {
-                            listB2.Items.Add(string.Format("{0,-15}", rdr[0])); ;
-                        }
-                    }
-                    rdr.Close();
-                }
-                catch
-                {
-
-                }
             }
+            else
+            {
+                MessageBox.Show("Введено не числовое значение!!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+
+        }
+
+        private void Button_Kolori_bolshe(object sender, RoutedEventArgs e)
+        {
+
+            conf = W_i_n();
+            int d;
+            if (int.TryParse(conf, out d))
+            {
+                Tabler(new SqlCommand($"SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность > {conf}", conect));
+
+            }
+            else
+            {
+                MessageBox.Show("Введено не числовое значение!!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Button_Kolor_Diapazon_Click(object sender, RoutedEventArgs e)
+        {
+                    Window2 window = new Window2();
+                    window.Owner = this;
+                    window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    if (window.ShowDialog() == true)
+                    {
+                        conf = window.Menshe.Text;
+                        conf2 = window.Bolshe.Text;
+                    }
+
+                    int d, c;
+                    if (int.TryParse(conf, out d) & int.TryParse(conf2, out c))
+                    {
+                        
+                        Tabler(new SqlCommand($"SELECT Название, Колорийность FROM Table_O_F WHERE Колорийность < {conf} AND Колорийность > {conf2}", conect));
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введено не числовое значение!!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }        
+        }
+
+        private void Button_Red_Jelow_Click(object sender, RoutedEventArgs e)
+        {
+            Tabler(new SqlCommand("SELECT Название, Цвет FROM Table_O_F WHERE Цвет = 'Красный' AND Цвет = 'Желтый'", conect));
         }
     }
 }
+
+  
+
+
